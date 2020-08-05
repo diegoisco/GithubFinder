6d4ec9dcab52633630018011ed37bb62f5a32bb4
Diego Oruna <diego.orunac@gmail.com>
2020-08-05 10:02:26 -0500

    Initial commit

---
+++ GithubFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubFinder
+//
+//  Created by Diego Oruna on 5/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
